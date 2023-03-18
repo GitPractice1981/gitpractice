b9a571cf05936176b5704227eaca1b277af9fdec
Dinesh <dineshrawat1981@gmail.com>
2023-03-18 14:57:16 +0000

    hello.java

--- hello.java
+++ hello.java
@@ -2,8 +2,7 @@ class Hello
 {
 Public static void main(String args[])
 {
-HEAD
-System.out.println(“hi from central repo”);
+System.out.println(“hi from central reposity. This is my First commit”);
 >>>>>>> cfa4ffcd6c4fbe37f93925868bcab88cb940e1c8
 }
 }
